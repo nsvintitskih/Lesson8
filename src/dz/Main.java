dfdf730bf95590d2a05843bbfe3481d29869dcea
nsvintitskih <nsvintitskih@yandex.ru>
2023-10-18 01:26:03 +0300

    Lesson8

--- src/dz/Main.java
+++ src/dz/Main.java
@@ -1,2 +1,7 @@
-package dz;public class Main {
+package dz;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
 }
--- src/p1/Main.java
+++ src/p1/Main.java
@@ -1,5 +1,21 @@
+package p1;//change
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person person = new Person("Katy");//6
+        System.out.println(person.getName());//7
+        changePerson(person);   //13                                  //8 changeName(person);
+        System.out.println(person.getName());//9
+
+
+    }
+
+    static void changePerson(Person p){//10
+        p = new Person("Dasha");//11
+        p.setName("Tanya");//12;
+    }
+
+    static void changeName(Person person){          //4
+        person.setName("Masha");                   //5
     }
 }
